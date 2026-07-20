ec13e35c5e361b9d29e32608a14ac7632ec2df9e
biancka <biancka30ar@gmail.com>
2026-07-19 23:51:32 -0600

    feat: poligon 2

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,14 @@ fn main() {
     let mut image = Image::gen_image_color(image_width, image_height, Color::BLACK);
 
     let line_color = Color::WHITE;
-    let fill1 = Color::new(0xE6, 0xC8, 0x28, 255);
+    let fill2 = Color::new(0x3C, 0x8C, 0xDC, 255);
 
-    let polygon1: [(i32, i32); 10] = [
-        (165, 380), (185, 360), (180, 330), (207, 345), (233, 330),
-        (230, 360), (250, 380), (220, 385), (205, 410), (193, 383),
+    let polygon2: [(i32, i32); 4] = [
+        (321, 335), (288, 286), (339, 251), (374, 302),
     ];
 
-    fill_polygon(&mut image, &[&polygon1], fill1);
-    draw_polygon(&mut image, &polygon1, line_color);
+    fill_polygon(&mut image, &[&polygon2], fill2);
+    draw_polygon(&mut image, &polygon2, line_color);
 
     let output_file_name = "out.png";
     image.export_image(output_file_name);
